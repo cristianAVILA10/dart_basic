53ea6dc1eeeb266f716a8a95c9b7d9a6b11ecd8d
unknown <usuario1@M2-305-03-11362.utadeo.loc>
2024-02-08 19:29:35 -0500

    primer commit

---
+++ bin/introduccion.dart
@@ -0,0 +1,53 @@
+import 'dart:html';
+
+void main(List<String> arguments) {
+
+  String variableTexto = "Hola Mundo";
+  int numeroEntero = 15;
+  double numeroDouble = 3.7;
+  bool esVerdad = true;
+
+  var esUnTexto = "Texto";
+  dynamic esUnNumero = 14;
+  esUnNumero = "Prueba"; 
+
+  print(variableTexto);
+
+
+  String? name ;
+
+  var arreglos = [1,2,3,4,5,6,7,8,9];
+
+  List<int> lista = [1,2,3,4,6,7,9];
+
+  List<String> listaLetras = ["A","B","C","D","E"];
+
+  //Comentarios de una sola linea
+
+  /*
+    Comentarios de varias lineas
+  */
+
+  if (numeroEntero==15) {
+    print("La variable es igual a 15");
+  }else if(numeroEntero ==1){
+    print("La variable es igual a 1");
+  }else{
+    print("No sabemos el valor");
+  }
+
+  switch (numeroEntero) {
+    case 15:
+      print("La variable es igual a 15");
+      break;
+  }
+
+  for (int i = 0; i < numeroEntero; i++) {
+    
+  }
+
+  while(numeroEntero > 15){
+    print("El numero es menor a 15");
+  }
+  
+}
